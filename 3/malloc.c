43996365b6cfc826706f8aa9c76191e18a0dcb3c
Alejandro <alejandro.ocampo2@udea.edu.co>
2024-10-11 09:11:58 -0500

    Static memory allocate

---
+++ 3/malloc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+
+	char name[32];
+	printf("What is your name?\n");
+	scanf("%s", &name);
+	printf("Hello %s\n", name);
+	printf("Hello %s\n", &name);
+	return 0;
+}
