e4fc05f4760bff3e603a81792cdf39a72b80235e
it <it@localhost.localdomain>
2024-10-22 22:46:20 -0500

    pointers

---
+++ pointers/pointers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ()
+{
+	int a = 5;
+	int *p;
+	p = &a;
+	printf("%p\n", a);
+	printf("%p\n", p);
+	printf("%d\n", a);
+	printf("%d\n", *p);
+	while(1)
+	{
+	
+	}
+	return 0;
+}
